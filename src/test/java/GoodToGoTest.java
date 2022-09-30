8aefe5a50abbed94baf91ba9b04978b5870069dd
Paul Duncan <paul.duncan@agfa.com>
2022-09-30 10:25:46 -0400

    Added hamcrest

--- src/test/java/GoodToGoTest.java
+++ src/test/java/GoodToGoTest.java
@@ -2,6 +2,8 @@ import java.util.List;
 
 import org.junit.jupiter.api.Test;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasSize;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class GoodToGoTest {
@@ -12,5 +14,10 @@ class GoodToGoTest {
                 .hasSize(3)
                 .containsExactly("One", "Two", "Three")
                 .doesNotContain("Four");
+    }
+
+    @Test
+    void iCanRunTestsWithHamcrest(){
+        assertThat(List.of("One", "Two", "Three"),hasSize(3));
     }
 }
